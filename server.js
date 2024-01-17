797ac107bd55195e594337b517bdc46182a1a921
aindriu80 <aindriu80@gmail.com>
2024-01-17 13:29:22 +0000

    Adding loader, specifying details on api request

--- server.js
+++ server.js
@@ -12,21 +12,28 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Handle GET request to fetch users
-app.get("/users", (req, res) => {
-  const users = [
-    { id: 1, name: "John Does" },
-    { id: 2, name: "Jake Wons" },
-    { id: 3, name: "Paul Somes" },
-  ];
+app.get("/users", async (req, res) => {
+  // const users = [
+  //   { id: 1, name: "John Does" },
+  //   { id: 2, name: "Jake Wons" },
+  //   { id: 3, name: "Paul Somes" },
+  // ];
 
-  res.send(`
+  setTimeout(async () => {
+    const limit = +req.query.limit || 10;
+    const response = await fetch(
+      `https://jsonplaceholder.typicode.com/users?_limit=${limit}`,
+    );
+    const users = await response.json();
+
+    res.send(`
 <h1 class="text-2xl font-bold my-4">Users</h1>
 <ul>
 ${users.map((user) => `<li>${user.name}</li>`).join("")}
 </ul>
 `);
+  }, 3000);
 });
-
 // Start the server
 app.listen(3000, () => {
   console.log("Server listening on port 3000");
